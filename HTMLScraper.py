157ce698cb98ad92f34d5402a04824295cdc913a
Steven Huff <steven.huffai@gmail.com>
2021-10-03 15:55:56 -0400

    Updating HTML Scraper

--- HTMLScraper.py
+++ HTMLScraper.py
@@ -1,8 +1,27 @@
 import requests
-import sys
+import argparse
 
-webpage = sys.argv[1]
+
+parser = argparse.ArgumentParser(usage='This program will send out an http request to access the content of the webpage you\'ve provided',)
+parser.add_argument('webpage', type=str, help='Enter the URL of the webpage to send the http request to')
+args = parser.parse_args()
+
+webpage = args.webpage
+
+if not (webpage.startswith('http://') or webpage.startswith('https://')):   
+    webpage = 'http://' + webpage
 
 response = requests.get(webpage)
-#import pdb;pdb.set_trace()
-print(response.content)
+print(response.headers)
+
+#bring in data
+
+response_headers = response.headers
+
+#loop through dict data for providing keys and values in text formate
+for key,value in response_headers.items():
+    # import pdb; pdb.set_trace()
+    print(key, value)
+
+#brand new flask server
+#
